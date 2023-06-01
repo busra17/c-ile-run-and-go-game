39e43ad38c5ad073adeaa1d24090585ec2703f16
busra17 <112686386+busra17@users.noreply.github.com>
2023-06-01 14:12:38 +0300

    c ile oyun kodları
    oyunda kullandığım kodların bulunduğu c# dosyalarım

---
+++ run and go game kod dökümantasyonu/CharacterController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    private Rigidbody2D r2d;
+    private Animator _animator;
+    private Vector3 charPos;
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private GameObject _camera;
+    private int sayi;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        r2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        charPos = transform.position;
+        sayi = 1;
+    }
+
+    private void FixedUpdate() //fizik hesaplamalarý bu kýsýmda yapýlýr.þuanda50kere yapýcak
+    {
+        //Debug.Log("Fixed"+sayi);
+        sayi = 2;
+        // r2d.velocity = new Vector2(x: speed, y: 0f);
+    }
+
+
+
+    void Update()
+    {
+
+        charPos = new Vector3(x: charPos.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime), charPos.y);
+        transform.position = charPos;//hesaplanýlan pozifyon karaktere iþlensin
+        if (Input.GetAxis("Horizontal") == 0.0f)
+        {
+            _animator.SetFloat("speed", value: 0.0f);
+        }
+
+        else
+        {
+            _animator.SetFloat("speed", value: 1.0f);
+
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        else if (Input.GetAxis("Horizontal") < -0.01f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+           
+            sayi = 3;
+            Debug.Log("Update" + sayi);
+    }
+
+    private void LateUpdate()
+    {
+        sayi=4;
+        //_camera.transform.position = new Vector3(charPos.x, y:charPos.y,z:charPos.z - 1.0f);//kemaranýn karakterden uzaklaþmasýný saðlýyor.
+    }
+
+
+}
